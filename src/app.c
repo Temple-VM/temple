6c9301d277f88560fa42350c6fe2611c2f4b13de
LordOfTrident <lordoftrident@gmail.com>
2022-08-21 22:31:26 +0200

    Fixed usage and version function

--- src/app.c
+++ src/app.c
@@ -2,23 +2,27 @@
 
 void help(void) {
 	puts(" _..__ ________  _________ _      _____\n"
-	     "|_|| _|  ___|  \\/\\_|| ___ \\ |    |  __/\n"
-	     "  ||| | |__ | . \\._/| |,/ /\\\\    | |__ \n"
-	     "  | |/|  _||| |\\/|_||  \\|/| \\|   |  _||\n"
-	     " .| ||| |_``| | / _|| ||| | ||.._| |_||\n"
-	     " \\\\_/\\\\____/\\_| ||_/\\_|   \\___||/\\____/\n"
-	     " ||   |/        ||            //     ||\n"
-	     "\n"
-	     "Github: https://github.com/Temple-VM/temple\n"
-	     "\n"
-	     "Usage: temple [OPTIONS] [FILE [ARG...]]\n"
-	     "OPTIONS:\n"
-	     "  -h, --help,    - show this message\n"
-	     "  -v, --version  - show the version");
+		"|_|| _|  ___|  \\/\\_|| ___ \\ |    |  __/\n"
+		"  ||| | |__ | . \\._/| |,/ /\\\\    | |__ \n"
+		"  | |/|  _||| |\\/|_||  \\|/| \\|   |  _||\n"
+		" .| ||| |_``| | / _|| ||| | ||.._| |_||\n"
+		" \\\\_/\\\\____/\\_| ||_/\\_|   \\___||/\\____/\n"
+		" ||   |/        ||            //     ||\n"
+		"\n"
+		"Github: https://github.com/Temple-VM/temple\n"
+		"\n"
+		"Usage: temple [OPTIONS] [FILE [ARG...]]\n"
+		"OPTIONS:\n"
+		"  -h, --help,    - show this message\n"
+		"  -v, --version  - show the version");
+
+	exit(0);
 }
 
 void version(void) {
 	printf("Temple %i.%i.%i\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH);
+
+	exit(0);
 }
 
 void app_read_args(app_t *p_app, int p_argc, char **p_argv) {
